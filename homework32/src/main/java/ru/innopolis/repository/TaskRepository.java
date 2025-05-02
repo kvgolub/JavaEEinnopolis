d8f0ee78a2ff5731e94566d371720d8de2539fdb
k.golub <k.golub@mail.ru>
2025-05-03 00:18:25 +0200

    homeworks/homework32: JUnit 5, Spring Test и Spring Boot Test
    Реализовано покрытие тестами сервисного слоя приложения с помощью JUnit5 и Spring Boot Test.
    За основу взят проект ДЗ «Spring Security»:
    1. Добавлен Spring Boot Test в Maven зависимости.
    2. Протестированы методы слоя Сервисы с помощью Spring Boot Test, JUnit5. Осуществлена проверка покрытия
    тестами с помощью стандартного плагина "Code Coverage for Java" в IntelliJ IDEA.
    3. Для тестов использованы файлы с входными данными (testParamCreate.csv, testParamDelete.csv) и аннотации
    параметризированных тестов (@ParameterizedTest, @CsvFileSource).
    4. Входные файлы для тестирования перенести в test/resources.
    5. Сделаны скриншоты проекта.

---
+++ homework32/src/main/java/ru/innopolis/repository/TaskRepository.java
@@ -0,0 +1,19 @@
+package ru.innopolis.repository;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.innopolis.entity.Task;
+
+
+/**
+ * Слой Репозитория
+ *
+ * <p>Доступ к Базе данных через JPA</p>
+ *
+ * @author K.Golub
+ * @version 1.0
+ */
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
