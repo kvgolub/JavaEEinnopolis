f2ddddeb2a01ac13598c9106a4e5d04e2e15c638
k.golub <k.golub@mail.ru>
2025-06-02 23:28:12 +0200

    homeworks/homework28: JWT (JSON Web Tokens).
    Взят за основу проект ДЗ 354, который реализует API для работы с сущностью Task:
    1. Убраны все компоненты, связанные с BasicAuth, включая настройки в SecurityConfig.
    2. Оставлена единственная учетная запись "admin".
    3. Создан класс JwtRequestFilter для проверки токена в заголовке Authorization.
    4. Сгенерирован JWT-токен через библиотеку jjwt (подключены как зависимости MAVEN).
    5. Осуществлена проверка токена при запросе к эндпоинтам задач.
    6. Сделаны скриншоты проекта.

---
+++ homework28/src/main/java/ru/innopolis/controller/GenerateTokenController.java
@@ -0,0 +1,16 @@
+package ru.innopolis.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/get_token")
+public interface GenerateTokenController {
+
+    @GetMapping("/{username}")
+    ResponseEntity<String> generateJwt(@PathVariable String username);
+}
